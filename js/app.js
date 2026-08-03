2496febe4fa58f990cd29d0fae8055a523740ca4
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-08-03 18:07:10 +0530

    Add image upload button functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log("ElectroVolt Diagram Studio Started");
+// Image Upload Button
+
+const imageBtn = document.getElementById("imageBtn");
+const imageInput = document.getElementById("imageInput");
+
+imageBtn.addEventListener("click", function () {
+
+    imageInput.click();
+
+});
