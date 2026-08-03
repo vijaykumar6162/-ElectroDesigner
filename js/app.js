6958cb11c75688530fc0b77a0f062580e3de648b
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-08-03 18:10:13 +0530

    Implement image upload and display feature
    Added functionality to upload and display images on the canvas.

--- js/app.js
+++ js/app.js
@@ -9,3 +9,36 @@ imageBtn.addEventListener("click", function () {
     imageInput.click();
 
 });
+// Image Upload
+
+imageInput.addEventListener("change", function () {
+
+    const file = this.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+
+        const img = document.createElement("img");
+
+        img.src = e.target.result;
+
+        img.style.position = "absolute";
+
+        img.style.left = "100px";
+
+        img.style.top = "100px";
+
+        img.style.width = "150px";
+
+        img.style.cursor = "move";
+
+        document.getElementById("drawingCanvas").appendChild(img);
+
+    };
+
+    reader.readAsDataURL(file);
+
+});
