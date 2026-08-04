c8874498ed775aac1ca353c756a8608fba2796c2
vijaykumar6162 <vijaykumarmandal01106162@gmail.com>
2026-08-04 07:10:59 +0530

    Add event listener for canvas click in wire.js

--- js/wire.js
+++ js/wire.js
@@ -1,1 +1,19 @@
+// =========================
+// Wire System
+// =========================
 
+console.log("Wire Loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const canvas = document.getElementById("drawingCanvas");
+
+    canvas.addEventListener("click", function () {
+
+        if (window.currentTool !== "wire") return;
+
+        alert("Canvas Ready For Wire");
+
+    });
+
+});
